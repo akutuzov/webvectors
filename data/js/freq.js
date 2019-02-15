b7df5be135387e30dd231e0cf50e9f77ff5601e8
Nastasia <lisitsinan@gmail.com>
2019-02-15 20:49:16 +0300

    Update freq.js to iterate from frequency values

--- data/js/freq.js
+++ data/js/freq.js
@@ -1,17 +1,11 @@
 $(document).ready(function(){
-    $("#high").click(function(){
-      if ($(this).is(':checked')){
-        $("li.high").fadeIn('slow');
-      } else {$("li.high").fadeOut('slow');}
-    });
-    $("#mid").click(function(){
-      if ($(this).is(':checked')){
-        $("li.mid").fadeIn('slow');
-      } else {$("li.mid").fadeOut('slow');}
-    });
-    $("#low").click(function(){
-      if ($(this).is(':checked')){
-        $("li.low").fadeIn('slow');
-      } else {$("li.low").fadeOut('slow');}
+  var freq = ['high', 'mid', 'low']
+    $.each(freq, function(index,value){
+      $("#"+value).click(function(){
+        if ($(this).is(':checked')){
+          $("li."+value).fadeIn('slow');
+        } else {$("li."+value).fadeOut('slow');}
+      });
     });
   });
+  
