90ce08515354079f54818efef98ab9d7c17cd1c2
Nastasia <lisitsinan@gmail.com>
2019-11-20 23:38:03 +0300

    Fix problem with difference between q and qf

--- word2vec_server.py
+++ word2vec_server.py
@@ -185,7 +185,7 @@ def find_synonyms(query):
         results['frequencies'][res[0]] = (freq, tier)
     raw_vector = model[qf]
     results['vector'] = raw_vector.tolist()
-    results['edges'] = get_edges(qf, model, results['neighbors'])
+    results['edges'] = get_edges(q, model, results['neighbors'])
     return results
 
 
